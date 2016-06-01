840b1ea959189b4fc07bba537bc91a6ba1db5cb7
David C. Villarreal <redhulk@rocketmail.com>
2016-06-01 12:43:25 -0500

    add files runtime.txt, Procfile, dump_auth.json, dump_sc.json, requirements.txt

--- Watch_Tower/Watch_Tower/settings_production.py
+++ Watch_Tower/Watch_Tower/settings_production.py
@@ -1,4 +1,25 @@
 from .settings import *
+import dj_database_url
 
 DEBUG = False
 ALLOWED_HOSTS = ['127.0.0.1']
+
+# Update database configuration with $DATABASE_URL.
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
+
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
+
+# Simplified static file serving.
+# https://warehouse.python.org/project/whitenoise/
+
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+SECRET_KEY = os.environ.get("g^4n3m-cxg-h3ecgv9$ttan8kvw03y(1snc71^0fcy(dje85sz")
