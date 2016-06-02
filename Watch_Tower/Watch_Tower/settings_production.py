e22ed79db0130bc517e25650e97a27bb38f1048c
David C. Villarreal <redhulk@rocketmail.com>
2016-06-02 11:47:43 -0500

    adding files

--- Watch_Tower/Watch_Tower/settings_production.py
+++ Watch_Tower/Watch_Tower/settings_production.py
@@ -2,7 +2,9 @@ from .settings import *
 import dj_database_url
 
 #DEBUG = False
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1', '0.0.0.0']
+
+secret_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 # Update database configuration with $DATABASE_URL.
 db_from_env = dj_database_url.config(conn_max_age=500)
