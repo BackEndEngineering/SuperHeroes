d94f3a5b6eb180eb77d9d636080fb3424056f1af
David C. Villarreal <redhulk@rocketmail.com>
2016-06-02 11:58:15 -0500

    Update settings and add static dir for collectstatic

--- Watch_Tower/Watch_Tower/settings_production.py
+++ Watch_Tower/Watch_Tower/settings_production.py
@@ -4,7 +4,7 @@ import dj_database_url
 #DEBUG = False
 ALLOWED_HOSTS = ['127.0.0.1', '0.0.0.0']
 
-secret_KEY = os.environ.get('DJANGO_SECRET_KEY')
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 # Update database configuration with $DATABASE_URL.
 db_from_env = dj_database_url.config(conn_max_age=500)
